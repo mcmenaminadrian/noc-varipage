ce9d2ac3e2b7ddd0e0622e80f61199768f9f23ca
Adrian McMenamin <acm538@york.ac.uk>
2018-12-12 22:35:13 +0000

    working on multiple bus implementation

--- tree.cpp
+++ tree.cpp
@@ -21,63 +21,23 @@ Tree::Tree(Memory& globalMemory, Noc& noc, const long columns, const long rows)
 {
 	long totalLeaves = columns * rows;
 	levels = 0;
-	long muxCount = totalLeaves / 2;
+	long busCount = totalLeaves / 2;
 
 	//create the nodes
-	while (muxCount > 1) {
-		nodesTree.push_back(vector<Mux>(muxCount));
-		for (unsigned int i = 0; i < nodesTree[levels].size(); i++){
-			nodesTree[levels][i].assignGlobalMemory(&globalMemory);
-		}
+	while (busCount > 1) {
+		nodesTree.push_back(bus(levels, &globalMemory));
 		muxCount /= 2;
 		levels++;
 	}
 	//number the leaves
-	for (unsigned int i = 0; i < nodesTree[0].size(); i++)
-	{
-		nodesTree[0][i].assignNumbers(
-			i * 2, i * 2, i * 2 + 1, i * 2 + 1);
-		Tile *targetTile = noc.tileAt(i * 2);
-		Tile *targetTile2 = noc.tileAt(i * 2 + 1);
-		if (!targetTile || !targetTile2) {
-			cout << "Bad tile index: " << i << endl;
-			throw "tile index error";
-		}
-		targetTile->addTreeLeaf(&(nodesTree[0][i]));
-		targetTile2->addTreeLeaf(&(nodesTree[0][i]));
-	}
-	//root Mux - connects to global memory
-	nodesTree.push_back(vector<Mux>(1));
+	//root bus - connects to global memory
+	nodesTree.push_back(bus(levels, &globalMemory);
 	nodesTree[levels][0].assignGlobalMemory(&globalMemory);
-	nodesTree[levels][0].upstreamMux = nullptr;
-    nodesTree[levels][0].addMMUMutex();
-	for (int i = 0; i <= levels; i++) {
-		for (unsigned int j = 0; j < nodesTree[i].size(); j++) {
-			if (i > 0) {
-				nodesTree[i][j].downstreamMuxLow = 
-					&(nodesTree[i - 1][j * 2]);
-				nodesTree[i][j].downstreamMuxHigh = 
-					&(nodesTree[i - 1][j * 2 + 1]);
-			}
-			if (i < levels) {
-				nodesTree[i][j].upstreamMux = &(nodesTree[i + 1][j/2]);
-			}
-		}
-	}
-	//join the nodes internally
-	for (int i = 0; i < levels; i++)
-	{
-		for (unsigned int j = 0; j < nodesTree[i].size(); j+= 2)
-		{
-			nodesTree[i + 1][j / 2].joinUpMux(
-				nodesTree[i][j], nodesTree[i][j + 1]);
-		}
-	}
+	nodesTree[levels][0].upstreamBus = -1;
+	nodesTree[levels][0].addMMUMutex();
 	//initialise the mutexes
 	for (unsigned int i = 0; i < nodesTree.size(); i++) {
-		for (unsigned int j = 0; j < nodesTree[i].size(); j++) {
-			nodesTree[i][j].initialiseMutex();
-		}
+			nodesTree[i].initialiseMutex();
 	}
 
 	//attach root to global memory
